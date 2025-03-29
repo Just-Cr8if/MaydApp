35539fe7becba3055dad82b7fad9f4765e37f9b1
jimmyfl0 <jflorence0@gmail.com>
2025-03-29 14:37:29 -0400

    Mayd version for test flight

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import Bugsnag from '@bugsnag/expo';
-import BugsnagPluginReact from '@bugsnag/plugin-react';
 import Navigation from './src/components/Navigation';
 import { AuthProvider } from './src/context/AuthContext';
 import { RestaurantProvider } from './src/context/RestaurantContext';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-Bugsnag.start({
-  apiKey: 'eb7d47c970591295bfa8bbb7eae35d13',
-  appVersion: '1.0.0',
-  releaseStage: 'production',
-  plugins: [new BugsnagPluginReact()]
-});
-
-// Get the React error boundary from Bugsnag
-const ErrorBoundary = Bugsnag.getPlugin('react').createErrorBoundary(React);
-
 export default function App() {
   return (
-    <ErrorBoundary>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <RestaurantProvider>
-          <AuthProvider>
-            <Navigation />
-          </AuthProvider>
-        </RestaurantProvider>
-      </GestureHandlerRootView>
-    </ErrorBoundary>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <RestaurantProvider>
+        <AuthProvider>
+          <Navigation />
+        </AuthProvider>
+      </RestaurantProvider>
+    </GestureHandlerRootView>
   );
 }
